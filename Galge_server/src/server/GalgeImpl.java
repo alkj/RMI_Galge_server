8b515c0272b523924c1fcdcbb9d39cd4b34eca23
alexander <s165477@student.dtu.dk>
2019-03-12 11:46:22 +0100

    opdateret

--- Galge_server/src/server/GalgeImpl.java
+++ Galge_server/src/server/GalgeImpl.java
@@ -1,10 +1,12 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 package server;
 
+import brugerautorisation.transport.rmi.Brugeradmin;
+import java.rmi.Naming;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
@@ -15,11 +17,12 @@ import java.rmi.server.UnicastRemoteObject;
 public class GalgeImpl extends UnicastRemoteObject implements GalgeInterf {
     
     private GalgeLogik gl = new GalgeLogik();
+    private boolean erLoggetInd = false;
     
     public GalgeImpl() throws RemoteException{
         
     }
-
+    
     /**
      *
      * @throws Exception
@@ -30,38 +33,44 @@ public class GalgeImpl extends UnicastRemoteObject implements GalgeInterf {
         gl.nulstil();
         System.out.println("der er lige blevet startet et spil");
     }
-
+    
     @Override
     public String getOrd() throws java.rmi.RemoteException {
+        if (erLoggetInd) {            
         return gl.getSynligtOrd();
+        } else {
+            return "ikke logget ind";
+        }
     }
-
-    @Override
-    public void gætBogstav(String s) throws java.rmi.RemoteException {
-        gl.gætBogstav(s);
-        System.out.println(gl.logStatus());
-    }
-
+    
+    
     @Override
     public String gæt(String s) throws RemoteException {
+        
+        if (erLoggetInd) {            
         gl.gætBogstav(s);
         System.out.println("der er lige blevet gættet på bogstav :" + s);
         return gl.getSynligtOrd();
-
-
+        }
+        return "du er ikke logget ind";
+        
+        
     }
-
+    
     @Override
     public boolean varRigtigt() {
-        return gl.erSidsteBogstavKorrekt();
+        if (erLoggetInd) {
+        return gl.erSidsteBogstavKorrekt();            
+        }
+        return false;
     }
-
+    
     @Override
     public int antalFejl() throws RemoteException {
         System.out.println("spilleren har " + gl.getAntalForkerteBogstaver() + " forkerte gæt.");
         return gl.getAntalForkerteBogstaver();
     }
-
+    
     @Override
     public boolean erSpilletVundet() throws RemoteException {
         if (gl.erSpilletVundet()) {
@@ -69,7 +78,7 @@ public class GalgeImpl extends UnicastRemoteObject implements GalgeInterf {
         }
         return gl.erSpilletVundet();
     }
-
+    
     @Override
     public boolean erSpilletSlut() throws RemoteException {
         if (gl.erSpilletVundet()) {
@@ -82,11 +91,35 @@ public class GalgeImpl extends UnicastRemoteObject implements GalgeInterf {
         }
         return gl.erSpilletSlut();
     }
-
+    
     @Override
     public void startIgen() throws RemoteException {
         gl.nulstil();
         System.out.println("spillet er genstartet");
     }
+    
+    @Override
+    public boolean logInd(String brugerNavn, String kodeOrd) throws RemoteException {
+        
+        Brugeradmin ba;
+        String br = brugerNavn;
+        String ko = kodeOrd;
 
+        try {
+            ba = (Brugeradmin) Naming.lookup("rmi://javabog.dk/brugeradmin");
+            ba.hentBruger(br, ko);
+            System.out.println("--------logget ind--------");
+            this.erLoggetInd = true;
+            return this.erLoggetInd;
+        } catch (Exception e) {
+            System.err.println("ikke logget ind");
+            System.err.println("forkert brugernavn/kode");
+            this.erLoggetInd=false;
+            return this.erLoggetInd;
+            
+        }
+        
+    }
+    
+    
 }
