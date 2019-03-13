9833d5530dbcab975790d223f7bd142d94d26e87
alexander <s165477@student.dtu.dk>
2019-03-13 17:26:33 +0100

    nu kan man i det mindste ændre ord til et af nordfalks andre ord i GL

--- Galge_server/src/server/GalgeServer.java
+++ Galge_server/src/server/GalgeServer.java
@@ -16,19 +16,19 @@ import java.rmi.Remote;
 public class GalgeServer {
     
     public static void main(String[] arg) throws Exception {
-        /*
+        
         java.rmi.registry.LocateRegistry.createRegistry(1234); // start i server-JVM
         GalgeInterf g = new GalgeImpl();
         Naming.rebind("rmi://localhost:1234/galge", g);
         System.out.println("galge serveren er startet");
-        */
-        //rmi://130.225.170.204:5477/s165477
         
+        //rmi://130.225.170.204:5477/s165477
+        /*
         java.rmi.registry.LocateRegistry.createRegistry(5477); // start i server-JVM
         GalgeInterf g = new GalgeImpl();
         Naming.rebind("rmi://130.225.170.204:5477/s165477", g);
         System.out.println("galge serveren er startet");
-        
+        */
         /*
         serveren skal startes med følgende kommando:
         java -Djava.security.policy=security.policy -Djava.rmi.server.hostname=130.225.170.204 -Djava.rmi.server.codebase=file:/~/Galge_server.jar -jar Galge_server.jar 
